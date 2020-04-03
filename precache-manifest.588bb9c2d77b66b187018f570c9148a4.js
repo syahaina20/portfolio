6ec75d63f76fb1653a55d323fd0e966b2757b0cd
syahaina20 <syahaina20@gmail.com>
2020-04-03 12:03:00 +0800

    Updates

---
+++ precache-manifest.588bb9c2d77b66b187018f570c9148a4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb207e2165ebfedb004a1501639d5efa",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "968913c9f1b16feb8a1c",
+    "url": "/portfolio/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "88f5107854a9fb063ca8",
+    "url": "/portfolio/static/css/main.d13d29a0.chunk.css"
+  },
+  {
+    "revision": "968913c9f1b16feb8a1c",
+    "url": "/portfolio/static/js/2.90584091.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.90584091.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88f5107854a9fb063ca8",
+    "url": "/portfolio/static/js/main.9fd7c5a3.chunk.js"
+  },
+  {
+    "revision": "ccb8db86a30b109397cf",
+    "url": "/portfolio/static/js/runtime-main.cea968b2.js"
+  },
+  {
+    "revision": "143dbb6e6c586cddb975ec3a94264685",
+    "url": "/portfolio/static/media/flowers.143dbb6e.png"
+  },
+  {
+    "revision": "e31c65163eecd22a978555da096a92fd",
+    "url": "/portfolio/static/media/hogwarts.e31c6516.png"
+  },
+  {
+    "revision": "d4517b2ebcf791d19f5bfb8943d10bd7",
+    "url": "/portfolio/static/media/strangerthings.d4517b2e.png"
+  }
+]);
